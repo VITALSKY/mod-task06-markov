93b773e7c4ffbeb6da9f7bd825aaa680e46576a5
VITALSKY <Vitalsky17@gmail.com>
2021-05-04 01:18:49 +0300

    Vitalsky381807-2 update2

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,7 +6,7 @@ TEST(task1, test1)
 {
     vector<string> words{ "car", "a", "computer", "orange!" };
     Generatortxt G_object(words, 3, 1000);
-    ASSERT_STREQ("a computer orange!".c_str(), G_object.Generationtxt().c_str());
+    ASSERT_STREQ("a computer orange! ", G_object.Generationtxt().c_str());
 
 }
 
@@ -14,7 +14,7 @@ TEST(task2, test1)
 {
     vector<string> words{ "High", "Hi", "ho" };
     Generatortxt G_object(words, 3, 100);
-    ASSERT_STREQ("High Hi ho".c_str(), G_object.Generationtxt().c_str());
+    ASSERT_STREQ("High Hi ho ", G_object.Generationtxt().c_str());
 }
 
 TEST(task3, test1)
@@ -24,7 +24,7 @@ TEST(task3, test1)
     mma[{"throw", "rain"}].push_back("houston");
     mma[{"rain", "houston"}].push_back("last of all preffix");
     Generatortxt G_object(mma, 100);
-    ASSERT_STREQ("everything throw rain houston".c_str(), G_object.Generationtxt().c_str());
+    ASSERT_STREQ("everything throw rain houston ", G_object.Generationtxt().c_str());
 }
 
 TEST(task4, test1)
@@ -37,9 +37,9 @@ TEST(task4, test1)
     Generatortxt G_object(mma, 100);
     string output = G_object.Generationtxt();
     if (output[output[output.length() - 2]] != 'e')
-        ASSERT_STREQ("everything throw rain".c_str(), output.c_str());
+        ASSERT_STREQ("everything throw rain ", output.c_str());
     else
-        ASSERT_STREQ("everything throw move".c_str(), output.c_str());
+        ASSERT_STREQ("everything throw move ", output.c_str());
 }
 
 TEST(task5, test1)
